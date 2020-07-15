83eb48394457386dff674420e74c76571c065adc
Knika Dass <knikadass@gmail.com>
2020-07-15 22:47:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,14 +21,14 @@ def shipping_manifest
     "oil paintings"
     ]
   
-  shipping_manifest {
+  shipping_manifest = {
   "whale bone corsets" => 5,
    "porcelain vases" => 2,
    "oil paintings" => 3
     }
 
 
-end
+
 end
 
 def retrieval
