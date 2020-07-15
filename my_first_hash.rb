8b1c0cf91508e0aa8dbaa09813a2a4f80d335ba6
Knika Dass <knikadass@gmail.com>
2020-07-15 23:02:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,8 +17,8 @@ def retrieval
 end
 
 def adding
-shipping_manifest ["muskets"] = '2'
-shipping_manifest ["gun powder"] = '4'
+shipping_manifest ["muskets"] = "2"
+shipping_manifest ["gun powder"] = "4"
 
 end
 shipping_manifest
